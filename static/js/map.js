1815c3769f189b4fa643f6042ae83903fe955e7b
Michael Gagno <mgagno1@my.bcit.ca>
2021-05-11 21:14:52 -0700

    Fix default lng and zoom so it centers on Vancouver.

--- static/js/map.js
+++ static/js/map.js
@@ -5,8 +5,8 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 49.2827, lng: 123.1207 },
-    zoom: 8,
+    center: { lat: 49.2827, lng: -123.1207 },
+    zoom: 12,
   });
   console.log('Map loaded');
 }
