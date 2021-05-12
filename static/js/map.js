d01670cc6343252a53587b01b9d42354bc2af495
Michael Gagno <mgagno1@my.bcit.ca>
2021-05-11 21:12:19 -0700

    Display map on page.

---
+++ static/js/map.js
@@ -0,0 +1,12 @@
+// API code has been adapted from examples given in the Google Maps API documentation.
+// https://developers.google.com/maps/documentation/javascript/
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 49.2827, lng: 123.1207 },
+    zoom: 8,
+  });
+  console.log('Map loaded');
+}
