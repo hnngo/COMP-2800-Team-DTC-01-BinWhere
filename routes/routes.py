07ba86f82b4ead53120cf3501cb09e48880b27c4
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2021-05-11 22:14:51 -0700

    Add reusable Heading html template

--- routes/routes.py
+++ routes/routes.py
@@ -1,4 +1,4 @@
-from . import authentication_route, user_route, demo_route
+from . import authentication_route, user_route, demo_route, other_routes
 
 
 def initialize(app, db):
@@ -10,5 +10,4 @@ def initialize(app, db):
     authentication_route.init(app)
     user_route.init(app)
     demo_route.init(app, db)
-
-
+    other_routes.init(app)
