ac8a447039cef3977fd83e42f3a28f9f328512a3
Michael Gagno <mgagno1@my.bcit.ca>
2021-05-11 21:21:03 -0700

    Add basic pan to location button.

--- static/js/map.js
+++ static/js/map.js
@@ -1,12 +1,41 @@
 // API code has been adapted from examples given in the Google Maps API documentation.
 // https://developers.google.com/maps/documentation/javascript/
 
-let map;
+let map, infoWindow;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 49.2827, lng: -123.1207 },
-    zoom: 12,
-  });
-  console.log('Map loaded');
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 49.2827, lng: -123.1207 },
+        zoom: 12,
+    });
+    console.log('Map loaded');
+
+    infoWindow = new google.maps.InfoWindow();
+    const locationButton = document.createElement("button");
+    locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("Location found.");
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    });
 }
