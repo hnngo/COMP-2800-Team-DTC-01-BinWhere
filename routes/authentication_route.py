113253663c19e57c44fa2296725ef1f547f24651
KAZUMA_KUNOGI <kunogikaz@gmail.com>
2021-05-14 09:26:58 -0700

    added route for signup

--- routes/authentication_route.py
+++ routes/authentication_route.py
@@ -5,11 +5,11 @@ import json
 
 def init(app, auth):
     @app.route('/login', methods=['GET'])
-    def get():
+    def get_login():
         return render_template("login.html")
 
     @app.route('/login', methods=['POST'])
-    def post():
+    def post_login():
         email = request.form['name']
         password = request.form['pass']
 
@@ -20,23 +20,20 @@ def init(app, auth):
             error_dict = json.loads(error.strerror)
             return error_dict["error"]["message"]
 
-        return render_template("login.html")
+    @app.route('/signup', methods=['GET'])
+    def get_signup():
+        return render_template("signup.html")
 
+    @app.route('/signup', methods=['POST'])
+    def post_signup():
+        email = request.form['name']
+        password = request.form['pass']
 
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            auth.get_account_info(user['idToken'])
+            return "Thank you for signup"
 
-
-
-
-
-
-    #
-    # @app.route('/signup', methods=['GET', 'POST'])
-    # def signup():
-    #     if request.method == 'POST':
-    #         email = request.form['name']
-    #         password = request.form['pass']
-    #
-    #         user = auth.create_user_with_email_and_password(email, password)
-    #         auth.get_account_info(user['idToken'])
-    #
-    #         return "Thank you for signup"
+        except requests.exceptions.HTTPError as error:
+            error_dict = json.loads(error.strerror)
+            return error_dict["error"]["message"]
