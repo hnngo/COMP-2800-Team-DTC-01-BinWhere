90a73f4dc50c76713584bd631f5eb2cfee4647d5
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2021-05-13 22:11:52 -0700

    Add plain navbar implementation

--- routes/demo_route.py
+++ routes/demo_route.py
@@ -78,6 +78,3 @@ def init(app, db):
             text += f"{doc.id} => {doc.to_dict()}\n"
         return text
 
-    @app.route("/sidebar", methods=["GET"])
-    def get_sidebar():
-        return render_template('sidebar-example.html')
--- routes/other_routes.py
+++ routes/other_routes.py
@@ -2,6 +2,18 @@ from flask import render_template
 
 
 def init(app):
+    @app.route("/", methods=["GET"])
+    def get_index():
+        return render_template("sidebar-example.html")
+
     @app.route("/about-us", methods=["GET"])
     def get_about_us():
         return render_template("about-us.html", title="About Us")
+
+    @app.route("/sidebar", methods=["GET"])
+    def get_sidebar():
+        return render_template("sidebar-example.html")
+
+    @app.route("/nav", methods=["GET"])
+    def get_nav():
+        return render_template("includes/navbar.html")
