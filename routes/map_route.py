54701b2173a4ef2a62e9364c253951be2b5c737e
Michael Gagno <mgagno1@my.bcit.ca>
2021-05-13 22:57:48 -0700

    Define map routes

--- routes/map_route.py
+++ routes/map_route.py
@@ -5,3 +5,7 @@ def init(app):
     @app.route("/map", methods=["GET"])
     def get_map():
         return render_template("map.html", title="Map")
+
+    @app.route("/map/bin-details", methods=["GET"])
+    def get_bin_details():
+        return render_template("bin-details.html", title="Details")
