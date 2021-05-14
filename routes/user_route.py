c23a81a9ad6cf903149b848ba576349bad6442ce
Subin_Moon <smoon44@my.bcit.ca>
2021-05-13 22:32:49 -0700

    Modify header and complete profile-page as a static template

--- routes/user_route.py
+++ routes/user_route.py
@@ -2,10 +2,6 @@ from flask import render_template
 
 
 def init(app):
-    @app.route("/auth", methods=["GET"])
-    def get_auth():
-        return "Authentication Route"
-
     @app.route("/profile", methods=["GET"])
     def get_profile_page():
         return render_template("profile-page.html", title="My Account", show_back=True)
