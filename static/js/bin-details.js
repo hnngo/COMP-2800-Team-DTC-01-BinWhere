1f199166f5596f24751d98a06afba50bf2fc203d
Michael Gagno <mgagno1@my.bcit.ca>
2021-05-18 00:11:40 -0700

    Implement reverse geocoding. Should refactor this later..

--- static/js/bin-details.js
+++ static/js/bin-details.js
@@ -1,17 +1,22 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-let lat, long, type;
+let type, address;
+let geoAPIStart = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
+let geoAPIEnd = '&key=AIzaSyCCHFhbJQACuCA70fcban9dr2GS8PuUiO8&result_type=street_address';
 
 $.getJSON('/static/json/test-coords.json', function (data) {
     $.each(data.records, function (key, data) {
         if (data.id === urlParams.get("id")) {
-            lat = data.lat;
-            long = data.long;
-            type = data.type
+            type = data.type;
+            $.getJSON(geoAPIStart + data.lat + ',' + data.long + geoAPIEnd, function (geoData) {
+                address = geoData.results[0].formatted_address;
+                console.log("Address:" + address);
+            }).then(function () {
+                $('#bin-details-location').append(address);
+            });
         }
     });
 }).then(function () {
     $('#bin-details-type').append(type);
-    $('#bin-details-location').append('(' + lat + ' ' + long + ')');
 });
