7c11b5da0dba76887cbcf35c24052881fe3900d8
Subin_Moon <smoon44@my.bcit.ca>
2021-05-20 14:02:08 -0700

    Add user session to profile page

--- static/js/profile-page.js
+++ static/js/profile-page.js
@@ -1,50 +1,84 @@
+showSpinner();
+let user_id = undefined;
 
+window.onload = async () => {
 
+    user_id = await getCurrentUserId();
+    console.log(user_id);
+    clearSpinner();
 
-// Delete the picture uploaded from the user account
-function deletePicture() {
-    let delete_confirm = document.getElementById("delete-button-modal");
+    if (user_id === null) {
+        return window.location.href = "/login";
+    }
 
-    document.getElementById("delete-btn").addEventListener("click", function (event) {
-    delete_confirm.style.display = "block";
-    });
-
-    // Close the modal by pressing X
-    document.getElementById("btn-close").addEventListener("click", function(event) {
-        delete_confirm.style.display = "none";
-    });
-
-    // Close the modal by pressing Close
-    document.getElementById("close-btn").addEventListener("click", function(event) {
-        delete_confirm.style.display = "none";
-    });
-
-    // Delete the picture from the screen
-    document.getElementById("confirm-delete").addEventListener("click", function(event) {
-   let garbage_pic = document.getElementById("picture1")
-    garbage_pic.remove();
-   delete_confirm.style.display = "none";
-    });
+    $(".container")[0].classList.remove("d-none");
 }
-deletePicture();
+
+// // Delete the picture uploaded from the user account
+// function deletePicture() {
+//
+//     if ($("#delete-button-modal") !== null) {
+//         // Click delete button to open the confirmation modal
+//         $("#delete-btn").addEventListener("click", event => {
+//             $("#delete-button-modal").style.display = "block";
+//         })
+//         // Close the modal by pressing X
+//         $("#btn-close").addEventListener("click", function(event) {
+//             $("#delete-button-modal").style.display = "none";
+//         });
+//
+//         // Close the modal by pressing Close
+//         $("#close-btn").addEventListener("click", function(event) {
+//             $("#delete-button-modal").style.display = "none";
+//         });
+//
+//         // Delete the picture from the screen
+//         $("#confirm-delete").addEventListener("click", function(event) {
+//             $("#picture1").remove();
+//             $("#delete-button-modal").style.display = "none";
+//         });
+//     }
+// }
 
 
-// Edit the user name
-function editUsername() {
-    let username = document.getElementById("user-name");
-    let input_username = document.getElementById("change-username");
-    let submit_username = document.getElementById("submit-new-username");
 
-    document.getElementById("edit-name-btn").addEventListener("click", function(event) {
-    username.style.display = "none";
-    input_username.style.display = "block";
-    submit_username.style.display = "block";
-    });
+// function deletePicture() {
+//     let delete_confirm = document.getElementById("delete-button-modal");
+//
+//     document.getElementById("delete-btn").addEventListener("click", function (event) {
+//     delete_confirm.style.display = "block";
+//     // });
 
-    submit_username.addEventListener("click", function(event) {
-        username.style.display = "block";
-        input_username.style.display = "none";
-        submit_username.style.display = "none";
-    })
-}
-editUsername();
+//     // Close the modal by pressing X
+//     document.getElementById("btn-close").addEventListener("click", function(event) {
+//         delete_confirm.style.display = "none";
+//     });
+//
+//     // Close the modal by pressing Close
+//     document.getElementById("close-btn").addEventListener("click", function(event) {
+//         delete_confirm.style.display = "none";
+//     });
+//
+//     // Delete the picture from the screen
+//     document.getElementById("confirm-delete").addEventListener("click", function(event) {
+//    let garbage_pic = document.getElementById("picture1")
+//     garbage_pic.remove();
+//    delete_confirm.style.display = "none";
+//     });
+// }
+
+
+// // Edit the user name
+// function editUsername() {
+//     $("#edit-name-btn").addEventListener("click", function(event) {
+//         $("#user-name").style.display = "none";
+//         $("#change-username").style.display = "block";
+//         $("#submit-new-username").style.display = "block";
+//     });
+//
+//     $("#submit-new-username").addEventListener("click", function(event) {
+//         $("#user-name").style.display = "block";
+//         $("#change-username").style.display = "none";
+//         $("#submit-new-username").style.display = "none";
+//     })
+// }
