7638918d667b1beb391c0a5c2a7be0284171517c
KAZUMA_KUNOGI <kunogikaz@gmail.com>
2021-05-20 16:42:32 -0700

    Implement re-upvote and re-downvote function

--- routes/feedback_route.py
+++ routes/feedback_route.py
@@ -17,11 +17,19 @@ def init(app, db):
         bin_ref = db.collection('bins').document(bin_id)
         bin_data = bin_ref.get()
 
+        current_who_upvote = bin_data.to_dict()['who_upvote']
         if bin_data.exists:
-            bin_ref.update({
-                'upvote': bin_data.to_dict()['upvote'] + 1,
-                'who_upvote': bin_data.to_dict()['who_upvote'] + [user_id]
-            })
+            if user_id in current_who_upvote:
+                current_who_upvote.remove(user_id)
+                bin_ref.update({
+                    'upvote': bin_data.to_dict()['upvote'] - 1,
+                    'who_upvote': current_who_upvote
+                })
+            else:
+                bin_ref.update({
+                    'upvote': bin_data.to_dict()['upvote'] + 1,
+                    'who_upvote': bin_data.to_dict()['who_upvote'] + [user_id]
+                })
             return jsonify({"error": 0})
         else:
             return jsonify({"error": "There's no such bin existed!"})
@@ -35,11 +43,20 @@ def init(app, db):
 
         bin_ref = db.collection('bins').document(bin_id)
         bin_data = bin_ref.get()
+        current_who_downvote = bin_data.to_dict()["who_downvote"]
+
         if bin_data.exists:
-            bin_ref.update({
-                'downvote': bin_data.to_dict()['downvote'] + 1,
-                'who_downvote': bin_data.to_dict()['who_downvote'] + [user_id]
-            })
+            if user_id in current_who_downvote:
+                current_who_downvote.remove(user_id)
+                bin_ref.update({
+                    'downvote': bin_data.to_dict()['downvote'] - 1,
+                    'who_downvote': current_who_downvote
+                })
+            else:
+                bin_ref.update({
+                    'downvote': bin_data.to_dict()['downvote'] + 1,
+                    'who_downvote': bin_data.to_dict()['who_downvote'] + [user_id]
+                })
             return jsonify({"error": 0})
         else:
             return jsonify({"error": "There is no such bin"})
