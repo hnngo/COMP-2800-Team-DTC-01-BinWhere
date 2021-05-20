530cd50202b4050114d006fe9a5786bdc3251800
Subin_Moon <smoon44@my.bcit.ca>
2021-05-20 16:16:27 -0700

    Get bin data from db on profile page

--- routes/user_route.py
+++ routes/user_route.py
@@ -1,28 +1,55 @@
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, session
 import base64
+import json
 
 
 def init(app, db, auth):
-    doc = db.collection("users").document("w90yooWPmSDk2O6VQ0BQ").get()
-
     @app.route("/profile", methods=["GET"])
     def get_profile_page():
-        # TODO: Replace this with the real user id session
-        if doc.exists:
-            data = doc.to_dict()
-            # TODO: get bin information and calculate reliability
-        else:
-            return redirect("/login")
-        return render_template("profile-page.html", title="My Account", show_back=True, data=data)
+        try:
+            user_id = db.collection("users").document(session.get("user_id")).get()
 
-    @app.route("/profile", methods=["POST"])
-    def modify_user_info():
+            profile_data = user_id.to_dict()
+            uploaded_bin = profile_data["uploaded_bin"]
+
+            bin_data = bin_data_array(uploaded_bin)  # a list of bin data in dict
+
+        except KeyError:
+            return redirect("/login")
+
+        return render_template("profile-page.html", title="My Account", show_back=True, profile_data=profile_data, postedbin_data=bin_data)
+
+    # Create a list of dicts of bin data
+    def bin_data_array(uploaded_bin: list) -> list:
+        result = []
+        for bin_id in uploaded_bin:
+            bin_data = db.collection("bins").document(bin_id).get().to_dict()
+            bin_data["reliability"] = calculate_reliability(bin_data["upvote"], bin_data["downvote"])
+            bin_data["date_created"] = bin_data["date_created"].strftime("%Y-%m-%d")
+            result.append(bin_data)
+        return result
+
+    # Calculate the reliability of bin
+    def calculate_reliability(upvote: int, downvote: int) -> float:
+        return round(upvote / (upvote + downvote) * 100)
+
+    @app.route("/profile/name", methods=["POST"])
+    def modify_user_name():
+        db.collection("users").document("w90yooWPmSDk2O6VQ0BQ").update({
+            "name": request.form["name"],
+        })
+        return render_template("profile-page.html", title="My Account", show_back=True, data=user_id.to_dict())
+
+    @app.route("/profile/avatar", methods=["POST"])
+    def modify_user_avatar():
         photo_obj = request.files["avatar"]
         encoded_string = "data:image/png;base64," + str(base64.b64encode(photo_obj.read()))[2:-1]
 
         db.collection("users").document("w90yooWPmSDk2O6VQ0BQ").update({
-            "name": request.form["name"],
             "avatar": encoded_string
         })
-        return render_template("profile-page.html", title="My Account", show_back=True, data=doc.to_dict())
+        return render_template("profile-page.html", title="My Account", show_back=True, data=user_id.to_dict())
 
+    # @app.route("/profile/bin", methods=["DELETE"])
+    # def delete_bin():
+    #     pass
