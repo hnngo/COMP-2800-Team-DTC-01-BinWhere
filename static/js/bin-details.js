1849c0637d0427f655a75c560fec3930a30c7949
KAZUMA_KUNOGI <kunogikaz@gmail.com>
2021-05-20 15:50:47 -0700

    Attach event to bindetail page

--- static/js/bin-details.js
+++ static/js/bin-details.js
@@ -10,3 +10,46 @@ function reverseGeoCode() {
         $('#bin-details-location').append(bin_address);
     });
 }
+
+// VOTING
+const upvote = document.getElementById("thumbs-up");
+const downvote = document.getElementById("thumbs-down");
+const urlParams = new URLSearchParams(window.location.search);
+const binId = urlParams.get('id');
+
+upvote.addEventListener('click', function() {
+    $.ajax({
+        url: "/upvote",
+        method: "POST",
+        data: JSON.stringify({
+            bin_id: binId
+        }),
+        contentType: "application/json",
+        success: function(response) {
+            if (response.error === 0) {
+                alert("Success!")
+            } else {
+                alert("Error!")
+            }
+        }
+    })
+})
+
+
+downvote.addEventListener('click', function() {
+    $.ajax({
+        url: "/downvote",
+        method: "POST",
+        data: JSON.stringify({
+            bin_id: binId
+        }),
+        contentType: "application/json",
+        success: function(response) {
+            if (response.error === 0) {
+                alert("Success!")
+            } else {
+                alert("Error!")
+            }
+        }
+    })
+})
