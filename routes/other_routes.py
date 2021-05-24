36449f100b5e31151f4fb6e300b900a0c719a14b
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2021-05-23 18:25:36 -0700

    Add 404 page implementation

--- routes/other_routes.py
+++ routes/other_routes.py
@@ -5,3 +5,7 @@ def init(app):
     @app.route("/about-us", methods=["GET"])
     def get_about_us():
         return render_template("about-us.html", title="About Us", show_back=True)
+
+    @app.errorhandler(404)
+    def get_404(_):
+        return render_template("404.html", title="Not Found")
