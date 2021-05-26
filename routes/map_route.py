fb0a432aedd60e7c407267900e39f97360a6bc9a
Michael Gagno <mgagno1@my.bcit.ca>
2021-05-25 21:12:14 -0700

    Show bin type icon on search results

--- routes/map_route.py
+++ routes/map_route.py
@@ -69,11 +69,12 @@ def init(app, db):
         name = doc.get("name")
         not_include = doc.get("not_include")
         waste_type = doc.get("type").lower()
+        waste_type_icon = utils.get_icons([waste_type])[0]
 
         closest_bin = get_closest_bin(lat, long, waste_type)
 
         return render_template("search-results.html", title=name, description=description, image=image,
-                               not_include=not_include, waste_type=waste_type, closest_bin=closest_bin)
+                               not_include=not_include, waste_type=waste_type_icon, closest_bin=closest_bin)
 
     def get_closest_bin(lat: str, long: str, waste_type: str) -> dict:
         """Get the id of the closest bin to the user's current location."""
